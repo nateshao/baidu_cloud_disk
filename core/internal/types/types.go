c3404472397103bddd2543296ae6aa2ec2e063a0
shaotj <shaotj@300624.cn>
2024-08-14 21:35:56 +0800

    分享记录

--- core/internal/types/types.go
+++ core/internal/types/types.go
@@ -36,3 +36,15 @@ type UserRegisterRequest struct {
 
 type UserRegisterReply struct {
 }
+
+type ShareBasicDetailRequest struct {
+	Identity string `json:"identity,optional"`
+}
+
+type ShareBasicDetailReply struct {
+	RepositoryIdentity string `json:"repository_identity"`
+	Name               string `json:"name"`
+	Ext                string `json:"ext"`
+	Size               int64  `json:"size"`
+	Path               string `json:"path"`
+}
